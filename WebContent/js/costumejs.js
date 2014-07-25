56e573868642b855a9bc6c53b13443d22bcce7cd
lyon515520 <lyon515520@gmail.com>
2014-07-25 17:32:20 +0200

    tenth commit

--- WebContent/js/costumejs.js
+++ WebContent/js/costumejs.js
@@ -2,6 +2,28 @@
 function callback(keyword){
     $(keyword).slideToggle("300");
   }
+//-----------------------------------------------------------------------------
+function change_arrow(){
+	var div_hide = document.getElementById('ml-body-motive-hide').style;
+	var arrow_up = document.getElementById('ml-motive-arrow-up').style;
+	var arrow_down = document.getElementById('ml-motive-arrow-down').style;
+	if(div_hide.display != "none"){
+		arrow_up.display = "block";
+		arrow_down.display = "none";
+	}
+	else if(div_hide.display == "none"){
+		arrow_up.display = "none";
+		arrow_down.display = "block";
+	}
+}
+//---------------------------------------------------------------------------the part to change the direction of arrows
+function style_test(){
+	var change = document.getElementById('ml-body-motive-show').style; 
+	if(change.display=="block"){
+		change.backgroundColor = "red";
+	}
+}
+
 function showout(){
 	var test = document.getElementById("test1");
 	if(test.style.display!="inline-block"){
--- WebContent/js/google-maps.js
+++ WebContent/js/google-maps.js
@@ -1,4 +1,5 @@
 	var map;
+	var map2;
 	function initialize() {
 	  var mapOptions = {
 	    zoom: 15,
@@ -6,6 +7,8 @@
 	  };
 	  map = new google.maps.Map(document.getElementById('google-maps'),
 	      mapOptions);
+	  map2 = new google.maps.Map(document.getElementById('google-maps2'),
+		      mapOptions);
 	}
 	
 	google.maps.event.addDomListener(window, 'load', initialize);
