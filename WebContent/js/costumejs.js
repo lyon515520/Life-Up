5b87cf82d36f097897731079de957d9fbc7f5cc2
lyon515520 <lyon515520@gmail.com>
2014-07-30 17:25:52 +0200

    eleveth commit

--- WebContent/js/costumejs.js
+++ WebContent/js/costumejs.js
@@ -3,20 +3,28 @@ function callback(keyword){
     $(keyword).slideToggle("300");
   }
 //-----------------------------------------------------------------------------
-function change_arrow(){
-	var div_hide = document.getElementById('ml-body-motive-hide').style;
-	var arrow_up = document.getElementById('ml-motive-arrow-up').style;
-	var arrow_down = document.getElementById('ml-motive-arrow-down').style;
-	if(div_hide.display != "none"){
-		arrow_up.display = "block";
-		arrow_down.display = "none";
+function change_arrow(keyword){
+	if($(keyword).hasClass('ml-body-arrow-down')){
+		$(keyword).removeClass('ml-body-arrow-down').addClass('ml-body-arrow-up');
+	}else{
+		$(keyword).removeClass('ml-body-arrow-up').addClass('ml-body-arrow-down');
 	}
-	else if(div_hide.display == "none"){
-		arrow_up.display = "none";
-		arrow_down.display = "block";
+}
+function change_arrow_float(keyword){
+	if($(keyword).hasClass('float-arrow-down')){
+		$(keyword).removeClass('float-arrow-down').addClass('float-arrow-up');
+	}else{
+		$(keyword).removeClass('float-arrow-up').addClass('float-arrow-down');
 	}
 }
 //---------------------------------------------------------------------------the part to change the direction of arrows
+function choose_day(keyword){
+	if($(keyword).hasClass('icon-mon-compte-week-off')){
+		$(keyword).removeClass('icon-mon-compte-week-off').addClass('icon-mon-compte-week-on');
+	}else{
+		$(keyword).removeClass('icon-mon-compte-week-on').addClass('icon-mon-compte-week-off');
+	}
+}
 function style_test(){
 	var change = document.getElementById('ml-body-motive-show').style; 
 	if(change.display=="block"){
@@ -64,3 +72,45 @@ function ml_show_hide_detail(keyword){
 	}
 
 }
+
+//Ajouter &agrave; mes lieux
+//Lieux favoris
+//Retirer de vos favoris
+/*function ml_mouseover_favoris(){
+	if(document.getElementById('ml-head-link-favoris').innerHTML == "Ajouter &agrave; mes lieux"){
+		document.getElementById('ml-head-link-favoris').style.color = "rgb(235,101,49)";
+		$("#ml-head-icon-favoris").attr("class" , 'icon-star-level ml-head-link-icon-click');
+	}
+	else if(document.getElementById('ml-head-link-favoris').innerHTML == "Lieux favoris"){
+		document.getElementById('ml-head-link-favoris').innerHTML = "Retirer de vos favoris";
+		document.getElementById('ml-head-link-favoris').style.color = "rgb(134,134,134)";
+		$("#ml-head-icon-favoris").attr("class" , 'icon-star-level ml-head-link-icon');
+	}
+	else {
+	}
+}
+function ml_mouseout_favoris(){
+	if(document.getElementById('ml-head-link-favoris').innerHTML == "Ajouter &agrave; mes lieux"){
+		document.getElementById('ml-head-link-favoris').style.color = "rgb(134,134,134)";
+		$("#ml-head-icon-favoris").attr("class" , 'icon-star-level ml-head-link-icon');
+	}
+	else if(document.getElementById('ml-head-link-favoris').innerHTML == "Lieux favoris"){
+	}
+	else {
+		document.getElementById('ml-head-link-favoris').innerHTML = "Lieux favoris";
+		document.getElementById('ml-head-link-favoris').style.color = "rgb(235,101,49)";
+		$("#ml-head-icon-favoris").attr("class" , 'icon-star-level ml-head-link-icon-click');
+	}
+}
+function ml_onclick_favoris(){
+	if(document.getElementById('ml-head-link-favoris').innerHTML != "Retirer de vos favoris"){
+		document.getElementById('ml-head-link-favoris').innerHTML = "Lieux favoris";
+		document.getElementById('ml-head-link-favoris').style.color = "rgb(235,101,49)";
+		$("#ml-head-icon-favoris").attr("class" , 'icon-star-level ml-head-link-icon-click');
+	}
+	else {
+		document.getElementById('ml-head-link-favoris').innerHTML = "Ajouter &agrave; mes lieux";
+		document.getElementById('ml-head-link-favoris').style.color = "rgb(134,134,134)";
+		$("#ml-head-icon-favoris").attr("class" , 'icon-star-level ml-head-link-icon');
+	}
+}*/
