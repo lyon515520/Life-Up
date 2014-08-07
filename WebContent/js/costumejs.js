13eb68d209f4353b815df43b7616eee8e737258b
lyon515520 <lyon515520@gmail.com>
2014-08-07 17:46:05 +0200

    17th commit

--- WebContent/js/costumejs.js
+++ WebContent/js/costumejs.js
@@ -121,3 +121,36 @@ function ml_onclick_favoris(){
 	}
 }*/
 
+/*                  keep the div always flow on the page, and on the top of the window
+ $.fn.smartFloat = function() {
+    var position = function(element) {
+        var top = element.position().top, pos = element.css("position");
+        $(window).scroll(function() {
+            var scrolls = $(this).scrollTop();
+            if (scrolls > top) {
+                if (window.XMLHttpRequest) {
+                    element.css({
+                        position: "fixed",
+	                    width: "685px",
+                        top: 0
+                    });    
+                } else {
+                    element.css({
+                        top: scrolls
+                    });    
+                }
+            }else {
+                element.css({
+                    position: "relative",
+                    top: top
+                });    
+            }
+        });
+    };
+    return $(this).each(function() {
+        position($(this));                         
+    });
+};
+
+$("#ville-search-float").smartFloat();
+*/
