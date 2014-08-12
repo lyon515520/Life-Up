dd54bd8c9b6fb837ded195bf1e564994544b9bba
lyon515520 <lyon515520@gmail.com>
2014-08-12 19:16:23 +0200

    20th commit

--- WebContent/js/sliderbar.js
+++ WebContent/js/sliderbar.js
@@ -11,3 +11,17 @@
     $( "#amount" ).val( $( "#slider-range" ).slider( "values", 0 ) + " ans" +
       " - " + $( "#slider-range" ).slider( "values", 1 ) + " ans" );
   });
+  
+  
+  $(function() {
+	    $( "#slider-range-min" ).slider({
+	      range: "min",
+	      value: 50,
+	      min: 1,
+	      max: 160,
+	      slide: function( event, ui ) {
+	        $( "#amount-min" ).val( ui.value + "km");
+	      }
+	    });
+	    $( "#amount-min" ).val( $( "#slider-range-min" ).slider( "value" ) + "km");
+	  });
