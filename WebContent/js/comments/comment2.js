8193e7dfcbb99c9f591eb1bff79d9a87f258c9c2
lyon515520 <lyon515520@gmail.com>
2014-08-14 16:43:03 +0200

    22thcomment

--- WebContent/js/comments/comment2.js
+++ WebContent/js/comments/comment2.js
@@ -1,11 +1,2 @@
-//var text = document.getElementById("mon-activity-comment");
-var text5 = document.getElementById("fg-comment-input1");
-autoTextarea(text5);
-//autoTextarea(text10);
-
-/*var comment_send = document.getElementsByTagName("input");
-for(var i=0;i<arr.length;i++){
-	   if(arr[i].id.indexOf("deviceCode")>-1){
-	    alert(arr[i].name+"\n"+arr[i].value);
-	   }
-	  }*/
+var text = document.getElementById("mon-activity-comment");
+autoTextarea(text);
